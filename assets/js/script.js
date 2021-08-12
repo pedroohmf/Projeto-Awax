5a301045bbb755eae8bc559bfb89b8a607d1a28d
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2021-08-11 22:29:33 -0300

    Projeto Awax

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var sliders = document.getElementById("sliders");
+let pointer = document.getElementById("pointer");
+var active = document.getElementById("active");
+document.getElementsByName
+var bolinha = false;
+
+function pSlide() {
+    pointer.click.style.backgroundColor
+    sliders.style.marginLeft = "0px";
+}
+
+
+    function slide1() {
+        sliders.style.marginLeft = "0px";
+    }
+
+    function slide2() {
+        sliders.style.marginLeft = "-100vw";
+    }
+
+    function slide3() {
+        sliders.style.marginLeft = "-200vw";
+    }
+
+    function slide4() {
+        sliders.style.marginLeft = "-300vw";
+    }
+
+    function slide5() {
+        sliders.style.marginLeft = "-400vw";
+    }
+    
+    
+
+
